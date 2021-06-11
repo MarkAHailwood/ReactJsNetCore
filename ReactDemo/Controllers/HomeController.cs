6da27b16e88bf6e82e0ac237c8f6c8ef5fbc3cc5
markhailwood <mark.hailwood@cognisess.com>
2021-06-11 09:13:22 +0100

    to master

---
+++ ReactDemo/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ReactDemo.Models;
+
+namespace ReactDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Author 1",
+                    Text = "Text 1"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Author 2",
+                    Text = "Text 2"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Author 3",
+                    Text = "Text 3"
+                }
+            };
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(_comments);
+        }
+
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ReactDemo/wwwroot/js/tutorial.jsx
@@ -0,0 +1,89 @@
+﻿
+
+class CommentForm extends React.Component {
+    render() {
+        return (
+            <div className="commentForm">Hello, world! I am a CommentForm.</div>
+        );
+    }
+}
+
+function createRemarkable() {
+    var remarkable =
+        'undefined' != typeof global && global.Remarkable
+            ? global.Remarkable
+            : window.Remarkable;
+
+    return new remarkable();
+}
+
+const data = [
+    { id: 1, author: 'a', text: 'f' },
+    { id: 2, author: 'b', text: 'd' },
+    { id: 3, author: 'c', text: 'e' }
+];
+
+class Comment extends React.Component {
+    rawMarkup() {
+        const md = new Remarkable();
+        const rawMarkup = md.render(this.props.children.toString());
+        return {__html: rawMarkup};
+    }
+
+    render() {
+        const md = createRemarkable();
+        return (
+            <div className="comment">
+                <h2 className="commentAuthor">{this.props.author}</h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
+            );
+    }
+}
+
+class CommentList extends React.Component {
+    render() {
+        const commentNodes = this.props.data.map(comment => (
+            <Comment author={comment.author} key={comment.id}>
+                {comment.text}
+                {comment.id}
+            </Comment>
+            ));
+        return <div className="commentList">{commentNodes}</div>
+    }
+}
+
+class CommentBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { data: [] };
+    }
+
+    loadCommentsFromServer() {
+        const xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = () => {
+            const data = JSON.parse(xhr.responseText);
+            this.setState({ data: data });
+        };
+        xhr.send();
+    }
+
+    componentDidMount() {
+        this.loadCommentsFromServer();
+        window.setInterval(
+            () => this.loadCommentsFromServer(),
+            this.props.pollInterval
+            );
+    }
+
+    render() {
+        return (
+            <div className="commentBox">
+                <CommentList data={this.state.data} />
+                <CommentForm />
+            </div>
+        );
+    }
+}
+ReactDOM.render(<CommentBox url="/comments" pollInterval={2000} />, document.getElementById('content'));
