bd28eb5898c9bb7d421f08e50607085f77e7a486
markhailwood <mark.hailwood@cognisess.com>
2021-06-13 18:00:31 +0100

    Add tictactoe framework

---
+++ ReactDemo/Controllers/TicTacController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReactDemo.Controllers
+{
+    public class TicTacController : Controller
+    {
+        public IActionResult TicTacToe()
+        {
+            return View();
+        }
+    }
+}
