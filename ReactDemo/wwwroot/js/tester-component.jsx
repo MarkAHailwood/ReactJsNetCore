272ca3e44d5c14a424ba713d93979ba02c879cbe
markhailwood <mark.hailwood@cognisess.com>
2021-06-14 08:42:53 +0100

    develop ttt to display x upon click (pass function into onlick value in component that updates state)

--- ReactDemo/wwwroot/js/tester-component.jsx
+++ ReactDemo/wwwroot/js/tester-component.jsx
@@ -1,8 +1,17 @@
 ﻿class Square extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            value: null,
+    };
+    }
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button
+                className="square"
+                onClick={() => this.setState({ value: 'X' })}
+                >
+                {this.state.value}
             </button>
         );
     }
@@ -10,7 +19,7 @@
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square />;
+        return <Square value={i}/>;
     }
 
     render() {
