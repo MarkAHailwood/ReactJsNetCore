c6ef05bc49a0e4eaddf1503ba4b82e29203b34f0
markhailwood <mark.hailwood@cognisess.com>
2021-06-17 08:42:25 +0100

    alternate x and o and monitor state value

--- ReactDemo/wwwroot/js/tester-component.jsx
+++ ReactDemo/wwwroot/js/tester-component.jsx
@@ -1,28 +1,30 @@
-﻿class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+﻿function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            xIsNext: true
         };
     }
 
     handleClick(i) {
-        const newSquares = this.state.squares.slice();
-        newSquares[i] = this.state.squares[i] == 'X' ? 'O' : 'X';
-        this.setState({squares: newSquares});
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -33,7 +35,7 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
