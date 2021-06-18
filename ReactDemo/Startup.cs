96c6821045a3b2b8e913a28d8c685d177eab80c9
markhailwood <mark.hailwood@cognisess.com>
2021-06-18 08:29:55 +0100

    Auto route to game, add function to check for winner and adjust handle click to retunr if there is a winner

--- ReactDemo/Startup.cs
+++ ReactDemo/Startup.cs
@@ -76,7 +76,7 @@ namespace ReactDemo
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=tictac}/{action=tictactoe}/{id?}");
             });
         }
     }
